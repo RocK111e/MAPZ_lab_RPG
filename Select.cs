1a85efef03230b4fa640f6f54235ce829217b897
RocK111e <romanmeg9@gmail.com>
2025-05-13 23:29:48 +0300

    before new project

--- Select.cs
+++ Select.cs
@@ -1,67 +1,23 @@
-// SelectScreen.cs
 using Godot;
+using System;
 
-public partial class SelectScreen : Control
+public partial class Select : Control
 {
 	public override void _Ready()
 	{
-		GD.Print("SelectScreen _Ready() CALLED."); // <-- ADD THIS LINE
-
-		Button archerButton = GetNode<Button>("CenterContainer/VBoxContainer/ArcherButton");
-		if (archerButton == null)
-		{
-			GD.PrintErr("ArcherButton NOT FOUND at path: CenterContainer/VBoxContainer/ArcherButton");
-		}
-		else
-		{
-			GD.Print("ArcherButton found. Connecting Pressed signal.");
-			archerButton.Pressed += () => OnButtonPressed("Archer");
-		}
-
-		Button swordsmanButton = GetNode<Button>("CenterContainer/VBoxContainer/SwordsmanButton");
-		if (swordsmanButton == null)
-		{
-			GD.PrintErr("SwordsmanButton NOT FOUND at path: CenterContainer/VBoxContainer/SwordsmanButton");
-		}
-		else
-		{
-			GD.Print("SwordsmanButton found. Connecting Pressed signal.");
-			swordsmanButton.Pressed += () => OnButtonPressed("Swordsman");
-		}
-
-		Button pirateButton = GetNode<Button>("CenterContainer/VBoxContainer/PirateButton");
-		if (pirateButton == null)
-		{
-			GD.PrintErr("PirateButton NOT FOUND at path: CenterContainer/VBoxContainer/PirateButton");
-		}
-		else
-		{
-			GD.Print("PirateButton found. Connecting Pressed signal.");
-			pirateButton.Pressed += () => OnButtonPressed("Pirate");
-		}
+		// Connect button signals to set the hero and change scene
+		GetNode<Button>("SelectScreen/CenterContainer/VBoxContainer/ArcherButton").Pressed += () => OnButtonPressed("Archer");
+		GetNode<Button>("SelectScreen/CenterContainer/VBoxContainer/SwordsmanButton").Pressed += () => OnButtonPressed("Swordsman");
+		GetNode<Button>("SelectScreen/CenterContainer/VBoxContainer/PirateButton").Pressed += () => OnButtonPressed("Pirate");
 	}
 
+		
 	private void OnButtonPressed(string hero)
 	{
-		GD.Print($"OnButtonPressed CALLED with hero: {hero}"); // <-- ADD THIS LINE
-
 		var global = GetNode<Global>("/root/Global");
-		if (global == null)
-		{
-			GD.PrintErr("Global singleton not found! Make sure it's autoloaded.");
-			return;
-		}
 		global.SelectedHero = hero;
-		GD.Print($"Selected Hero stored in Global: {global.SelectedHero}");
+		GD.Print($"Selected Hero: {global.SelectedHero}");
 
-		Error err = GetTree().ChangeSceneToFile("res://Main.tscn");
-		if (err != Error.Ok)
-		{
-			GD.PrintErr($"Error changing scene to Main.tscn: {err}");
-		}
-		else
-		{
-			GD.Print("Successfully initiated scene change to Main.tscn");
-		}
+		GetTree().ChangeSceneToFile("res://Main.tscn");
 	}
 }
