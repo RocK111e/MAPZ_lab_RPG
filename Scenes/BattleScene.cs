6129f739ec3e92151609f06566d6d11a3cb074b3
RocK111e <romanmeg9@gmail.com>
2025-05-16 22:31:12 +0300

    fixed methods for entities

--- Scenes/BattleScene.cs
+++ Scenes/BattleScene.cs
@@ -1,19 +1,26 @@
 // BattleScene.cs
 using Godot;
+using Scenes.Entity;
 
 public partial class BattleScene : Node2D // Or Control, or whatever your root node type is
 {
 	private Label _heroDisplayLabel;
+	private Node2D _heroDisplayNode;
 
 	public override void _Ready()
 	{
 		GD.Print("BattleScene ready!");
-		// Get reference to the label. Adjust path if needed.
 		_heroDisplayLabel = GetNode<Label>("HDL");
 		GD.Print($"Label found! {_heroDisplayLabel.Name}");
-		// Retrieve the selected hero name from our Autoload/Singleton
+
 		string selectedHero = GameData.SelectedHeroName;
 		GD.Print($"Selected hero: {selectedHero.ToString()}");
 		_heroDisplayLabel.Text = $"Selected Hero: {selectedHero}";
+
+		_heroDisplayNode = GD.Load<PackedScene>("res://Scenes/Entity.tscn").Instantiate<Node2D>();
+		AddChild(_heroDisplayNode);
+		_heroDisplayNode.Position = new Vector2(100, 100); // Set position as needed
+		_heroDisplayNode.Name = "HeroDisplayNode"; // Set a name for the node
 	}
+
 }
--- Scenes/Entity.cs
+++ Scenes/Entity.cs
@@ -1,30 +1,9 @@
 using Godot;
 
-public partial class Entitry : Node2D
+namespace Scenes.Entity
 {
-    public Label _nameLabel;
-    public Label _healthLabel;
-    public Sprite2D _sprite;
-    public ProgressBar _healthBar;
-
-    public override void _Ready()
+    public partial class Entity : Node2D
     {
-        _nameLabel = GetNode<Label>("NameLabel");
-        _healthLabel = GetNode<Label>("HealthLabel");
-        _sprite = GetNode<Sprite2D>("Sprite");
+       
     }
-
-    void set_name(string name)
-    {
-        _nameLabel.Text = name;
-    }
-
-    void set_health(int health, int maxHealth)
-    {
-        _healthLabel.Text = $"{health}/{maxHealth}";
-        _healthBar.Value = health;
-        _healthBar.MaxValue = maxHealth;
-    }
-    
-
 }
