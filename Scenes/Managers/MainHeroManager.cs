1c86ff78586212d491f38b708e4c27027fb70b4f
RocK111e <romanmeg9@gmail.com>
2025-05-21 20:25:28 +0300

    UI improvements

--- Scenes/Managers/MainHeroManager.cs
+++ Scenes/Managers/MainHeroManager.cs
@@ -12,17 +12,18 @@ class MainHeroManager
 	private ProgressBar _heroHealthBar;
 	private Label _heroHealthLabel;
 
-    public MainHeroManager(Node2D heroDisplayNode, string herName)
+    public MainHeroManager(Node2D heroDisplayNode, string heroName)
     {
         _heroDisplayNode = heroDisplayNode;
-        _heroName = herName;
+        _heroName = heroName;
         _heroNameLabel = heroDisplayNode.GetNode<Label>("Label");
         _heroNameLabel.Text = _heroName;
 
         _heroHealthBar = heroDisplayNode.GetNode<ProgressBar>("ProgressBar");
         _heroHealthLabel = heroDisplayNode.GetNode<Label>("ProgressBar/HPLabel");
-        
+
         _hero = MainHero.Instance;
+        _hero.HeroSelect(heroName);
 	}
 	public void ApplyDamage(int damage)
 	{
