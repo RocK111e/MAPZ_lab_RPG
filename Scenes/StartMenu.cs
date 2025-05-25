ee288a1598d04c60c158d5606844b8aab046bb69
RocK111e <romanmeg9@gmail.com>
2025-05-25 18:12:49 +0300

    StartMenu remake for new architecture

--- Scenes/StartMenu.cs
+++ Scenes/StartMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 using MAPZ_lab_RPG.Entities.Heroes;
 
@@ -9,33 +10,54 @@ public partial class StartMenu : Control
 
 	private const string BattleScenePath = "res://Scenes/BattleScene.tscn";
 
+	private List<string> heroNames = new List<string>
+	{
+		"Archer",
+		"Swordsman",
+		"Pirate",
+		"Wizard",
+		"Rogue",
+		"Paladin",
+		"Barbarian",
+	};
+
 	public override void _Ready()
 	{
 		GD.Print("StartMenu ready!");
-		
-		_archerButton = GetNode<Button>("CenterContainer/VBoxContainer/ArcherButton");
-		_swordsmanButton = GetNode<Button>("CenterContainer/VBoxContainer/SwordsmanButton");
-		_pirateButton = GetNode<Button>("CenterContainer/VBoxContainer/PirateButton");
-
-
-		_archerButton.Pressed += () => OnHeroSelected(HeroEnum.ARCHER);
-		_swordsmanButton.Pressed += () => OnHeroSelected(HeroEnum.SWORDSMAN);
-		_pirateButton.Pressed += () => OnHeroSelected(HeroEnum.PIRATE);
-		GD.Print("Binded buttons!");
-
-	}
-
-	private void OnHeroSelected(HeroEnum heroName)
-	{
-		GD.Print($"Hero selected: {heroName}");
-
-		GameData.SelectedHeroName = heroName;
-
-		// Change to the battle scene
-		var error = GetTree().ChangeSceneToFile(BattleScenePath);
-		if (error != Error.Ok)
+		var heroButtonContainer = GetNode<Control>("CenterContainer/VBoxContainer/ScrollContainer/HBoxContainer");
+		foreach (var heroName in heroNames)
 		{
-			GD.PrintErr($"Error changing scene to {BattleScenePath}: {error}");
+			GD.Print($"Available hero: {heroName}");
+			var buttonControl = GD.Load<PackedScene>($"res://Scenes/HeroSelectButton.tscn").Instantiate<CenterContainer>();
+			var button = buttonControl.GetNode<Button>("Button");
+			button.Text = heroName;
+			//heroButton.Pressed += () => OnHeroSelected(heroName);
+			heroButtonContainer.AddChild(buttonControl);
 		}
+
+		// _archerButton = GetNode<Button>("CenterContainer/VBoxContainer/ArcherButton");
+		// _swordsmanButton = GetNode<Button>("CenterContainer/VBoxContainer/SwordsmanButton");
+		// _pirateButton = GetNode<Button>("CenterContainer/VBoxContainer/PirateButton");
+
+
+		// _archerButton.Pressed += () => OnHeroSelected(HeroEnum.ARCHER);
+		// _swordsmanButton.Pressed += () => OnHeroSelected(HeroEnum.SWORDSMAN);
+		// _pirateButton.Pressed += () => OnHeroSelected(HeroEnum.PIRATE);
+		// GD.Print("Binded buttons!");
+
 	}
+
+	// private void OnHeroSelected(HeroEnum heroName)
+	// {
+	// 	GD.Print($"Hero selected: {heroName}");
+
+	// 	GameData.SelectedHeroName = heroName;
+
+	// 	// Change to the battle scene
+	// 	var error = GetTree().ChangeSceneToFile(BattleScenePath);
+	// 	if (error != Error.Ok)
+	// 	{
+	// 		GD.PrintErr($"Error changing scene to {BattleScenePath}: {error}");
+	// 	}
+	// }
 }
